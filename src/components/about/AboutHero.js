b404dabfbd59fb420357495fa426534fec650f06
prajapati-yash <77118917+prajapati-yash@users.noreply.github.com>
2023-03-30 16:26:16 +0530

    Created About Page Folder and started working on it

---
+++ src/components/about/AboutHero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../../styles/about/About.css'
+
+function AboutHero() {
+  return (
+    <>
+    <div className='container-fluid px-4 px-md-5'>
+        <div className='aboutHeroBg'>
+            <div>
+                  <h1>Hello</h1>
+            </div>
+
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default AboutHero
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import AboutHero from '../components/about/AboutHero'
 import Navbar from '../components/navbar/Navbar'
+import Footer from '../components/footer/Footer'
 
 function About() {
   return (
     <div>
       <Navbar/>
+      <AboutHero/>
+      <Footer/>
     </div>
   )
 }
