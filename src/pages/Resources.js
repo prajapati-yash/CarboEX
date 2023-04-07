22cbf2bc4f51adcd3bad16a952e55f79dca4f558
purvik6062 <purvikpanchal6336@gmail.com>
2023-04-07 17:44:45 +0530

    completed resources page

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ResourcesHero from '../components/resources/ResourcesHero'
 import ResourcesWIC from '../components/resources/ResourcesWIC'
+import ResourcesHCW from '../components/resources/ResourcesHCW'
+import ResourcesCalComp from '../components/resources/ResourcesCalComp'
+import ResourcesROP from '../components/resources/ResourcesROP'
 
 function Resources() {
   return (
     <div>
       <ResourcesHero />
       <ResourcesWIC />
+      <ResourcesHCW />
+      <ResourcesCalComp />
+      <ResourcesROP />
     </div>
   )
 }
